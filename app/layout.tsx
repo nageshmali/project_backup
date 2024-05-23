f7c9d9d931206a9c0b3ea19cf1bd10c4025d80ca
nageshmali <malinagesh60@gmail.com>
2024-05-23 13:49:13 +0530

    done with all stuff

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+import "./globals.css";
+import { Room } from "./Room";
+
+const workSans = Work_Sans({ 
+  subsets: ["latin"],
+  variable : '--font-work-sans',
+  weight: ['400', '600', '700']
+ });
+
+export const metadata: Metadata = {
+  title: "Picturifyy - A Design Application",
+  description: "Get you friends, create, edit, chat and export",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${workSans.className} bg-primary-grey-200`}>
+        <Room>
+          {children}
+        </Room>
+      </body>
+    </html>
+  );
+}
